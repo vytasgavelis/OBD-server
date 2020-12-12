2abf5628748c4fa83a0e7f4bc2c153e57092bec3
Vytas Gavelis <gavelisvytas@gmail.com>
2020-12-12 12:25:27 +0200

    add registration. only fetch speed tests from users who allow it

--- api.php
+++ api.php
@@ -23,6 +23,30 @@ try {
             }
         }
 
+        if ($_GET['action'] === 'register') {
+            if (isset($_POST['email'], $_POST['password'])) {
+                $output['success'] = false;
+                $errors = [];
+                if (strlen($_POST['password']) <= 8) {
+                    $errors[] = 'Slaptazodis turi susidaryti bent is 8 simboliu.';
+                }
+                if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                    $errors[] = 'Neteisingas el. pasto formatas';
+                }
+                if (is_array(findUserByEmail($connection, $_POST['email']))) {
+                    $errors[] = 'El. pastas jau uzimtas.';
+                }
+                if (count($errors) !== 0) {
+                    $output['errors'] = $errors;
+                } else {
+                    $output['success'] = insertUser(
+                        $connection, $_POST['email'],
+                        substr(hash('sha256', $_POST['password']), 5, 32)
+                    );
+                }
+            }
+        }
+
         if ($_GET['action'] === 'upload_speed_test') {
             if (isset($_POST['user_id'], $_POST['speed_test_data'])) {
                 $output['success'] = false;
@@ -58,16 +82,19 @@ try {
             if (isset($_GET['user_id'])) {
                 $settings = getUserSettings($connection, $_GET['user_id'])[0];
                 $output['success'] = true;
-                $output['settings'] = $settings;
+                $output['settings']['setting_id'] = $settings['id'];
+                $output['settings']['user_id'] = $settings['user_id'];
+                $output['settings']['can_compare_tests'] = $settings['can_compare_tests'] == 1 ? true : false;
             }
         }
 
         if ($_GET['action'] === 'update_user_settings') {
             $output['success'] = false;
             if (isset($_POST['user_id'], $_POST['can_compare_tests'])) {
-                $canCompareTests = $_POST['can_compare_tests'] === 'true' ? 1 : 0;
+                $canCompareTests = $_POST['can_compare_tests'] == 'true' ? 1 : 0;
                 updateUserSettings($connection, $_POST['user_id'], $canCompareTests);
                 $output['success'] = true;
+                $output['can_compare_tests'] = $canCompareTests;
             }
         }
 
@@ -102,7 +129,11 @@ function insertSpeedData($connection, int $userId, string $speedTestData): bool
 
 function fetchSpeedTests($connection): array
 {
-    $stmt = $connection->prepare('SELECT * FROM speed_test');
+    $stmt = $connection->prepare('
+        SELECT `speed_test`.`id`, `speed_test`.`user_id`, `speed_test`.`data` FROM `speed_test`
+        JOIN settings ON speed_test.user_id=settings.user_id
+        WHERE settings.can_compare_tests=1 
+    ');
     $stmt->execute([]);
 
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -110,7 +141,11 @@ function fetchSpeedTests($connection): array
 
 function fetchUserSpeedTests($connection, int $userId): array
 {
-    $stmt = $connection->prepare('SELECT * FROM speed_test WHERE user_id=?');
+    $stmt = $connection->prepare('
+        SELECT `speed_test`.`id`, `speed_test`.`user_id`, `speed_test`.`data` FROM `speed_test`
+        JOIN settings ON speed_test.user_id=settings.user_id
+        WHERE settings.can_compare_tests=1 AND settings.user_id=?
+    ');
     $stmt->execute([$userId]);
 
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -128,6 +163,22 @@ function updateUserSettings($connection, int $userId, int $canCompareTests)
 {
     $stmt = $connection->prepare('UPDATE `settings` SET `can_compare_tests` = ? WHERE user_id=?;');
     $stmt->execute([$canCompareTests, $userId]);
+
+    return true;
+}
+
+function findUserByEmail($connection, string $email)
+{
+    $stmt = $connection->prepare('SELECT * FROM user WHERE email = ?');
+    $stmt->execute([$email]);
+
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+function insertUser($connection, string $email, string $password): bool
+{
+    $stmt = $connection->prepare('INSERT INTO user (email, password) VALUES(?, ?)');
+    $stmt->execute([$email, $password]);
 
     return true;
 }
